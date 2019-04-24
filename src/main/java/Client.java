7e235e7ade39efa3c5a820d19e9c4685b83fe2ad
Marvin Zachary <acerzach@gmail.com>
2019-04-24 12:24:04 +0300

    added db connection and save

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -33,42 +33,42 @@ public class Client {
       this.getStylistId() == newClient.getStylistId();
     }
   }
-//   public static List <Client> all(){
-//     String sql = "SELECT id, name, stylistId FROM clients";
-//     try(Connection con = DB.sql2o.open()) {
-//       return con.createQuery(sql).executeAndFetch(Client.class);
-//     }
-//   }
+  public static List <Client> all(){
+    String sql = "SELECT id, name, stylistId FROM clients";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Client.class);
+    }
+  }
 
-//   public void save(){
-//     try(Connection con = DB.sql2o.open()) {
-//       String sql = "INSERT INTO clients (name, stylistId) VALUES (:name, :stylistId)";
-//       this.id = (int) con.createQuery(sql, true)
-//       .addParameter("name", this.name)
-//       .addParameter("stylistId", this.stylistId)
-//       .executeUpdate()
-//       .getKey();
-//     }
-//   }
+  public void save(){
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO clients (name, stylistId) VALUES (:name, :stylistId)";
+      this.id = (int) con.createQuery(sql, true)
+      .addParameter("name", this.name)
+      .addParameter("stylistId", this.stylistId)
+      .executeUpdate()
+      .getKey();
+    }
+  }
 
-//   public static Client find (int id){
-//     try(Connection con = DB.sql2o.open()) {
-//       String sql = "SELECT * FROM clients WHERE id=:id";
-//       Client client = con.createQuery(sql)
-//       .addParameter("id", id)
-//       .executeAndFetchFirst(Client.class);
-//       return client;
-//     }
-//   }
+  public static Client find (int id){
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM clients WHERE id=:id";
+      Client client = con.createQuery(sql)
+      .addParameter("id", id)
+      .executeAndFetchFirst(Client.class);
+      return client;
+    }
+  }
 
-//   public void update(String name) {
-//    try(Connection con = DB.sql2o.open()) {
-//      String sql = "UPDATE clients SET name = :name WHERE id = :id";
-//      con.createQuery(sql)
-//      .addParameter("name", name)
-//      .addParameter("id", id)
-//      .executeUpdate();
-//    }
-//  }
+  public void update(String name) {
+   try(Connection con = DB.sql2o.open()) {
+     String sql = "UPDATE clients SET name = :name WHERE id = :id";
+     con.createQuery(sql)
+     .addParameter("name", name)
+     .addParameter("id", id)
+     .executeUpdate();
+   }
+ }
 
-// }
+}
