a689d3f2c0c24508b5aba56dc5f8a08e48f38657
Marvin Zachary <acerzach@gmail.com>
2019-04-24 12:07:35 +0300

    Added imports and declared class App.java and set the port

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,21 +1,21 @@
-// import java.util.HashMap;
-// import java.util.Map;
-// import spark.ModelAndView;
-// import spark.template.velocity.VelocityTemplateEngine;
-// import static spark.Spark.*;
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
 
-// public class App {
-//   public static void main(String[] args) {
+public class App {
+  public static void main(String[] args) {
 
-//     ProcessBuilder process = new ProcessBuilder();
-//     Integer port;
-//     if (process.environment().get("PORT") != null) {
-//       port = Integer.parseInt(process.environment().get("PORT"));
-//     } else {
-//       port = 4567;
-//     }
+    ProcessBuilder process = new ProcessBuilder();
+    Integer port;
+    if (process.environment().get("PORT") != null) {
+      port = Integer.parseInt(process.environment().get("PORT"));
+    } else {
+      port = 4567;
+    }
 
-//     setPort(port);
+    setPort(port);
 
 
 //     staticFileLocation("/public");
